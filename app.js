55323275ada59520dc8492a2c9206ed7fd4a5db7
Pheelass <phelagleetz@gmail.com>
2026-09-18 16:31:25 -0300

    app.js update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import fs from 'node:fs/promises'
+import * as fs from 'node:fs/promises'
 const app = express();
 import { fileURLToPath } from "node:url";
 import path from "node:path";
+import bcrypt from "bcrypt"
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -16,13 +17,31 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + '/client/main.html');
 })
 
+const saltRounds = 5;
+
 app.get('/login/:username/:password', async (req, res) => {	
 	let ui = {
 		"tem paes?":"ent tchaes"
 	}
-	res.type('text').send("ain bolsonaru")
+	res.type('text').send("ain bolsonaru");
+	let file = './server/users.json'
+	const texto = await fs.readFile(file);
+	const dados = await JSON.parse(texto, 'utf8');
+	let usuario = req.params.username;
+
+	const hash = await bcrypt.hash( req.params.password, saltRounds )
+
+	dados.push({
+		nome: usuario, 
+		senha: hash
+	});
+
+	console.log(dados)
+
+	await fs.writeFile(file, JSON.stringify(dados, null, 2))	
 });
 
 app.listen('8000', '127.0.0.1', () => {
 	console.log('listening on http://localhost:8000')
 })
+
